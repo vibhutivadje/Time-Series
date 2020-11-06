6c678c51d11a94082a9004695b0a0d79a5f3e52d
vibhutivadje <vibhutivadje@gmail.com>
2020-11-06 12:49:29 -0800

    Partition data

--- Assignment1.R
+++ Assignment1.R
@@ -4,6 +4,7 @@ install.packages("forecast")
 library(forecast)
 
 ##Q1:
+##a)
 ## CREATE DATA FRAME. 
 #Grocery.data <- read.csv("673_case1.csv")
 Grocery.data <- read.csv(file.choose(), header = TRUE)
@@ -17,7 +18,7 @@ sales.ts <- ts(Grocery.data$Sales,
 
 sales.ts
 
-##Q2:
+##b):
 ## Use plot() to plot time series data  
 plot(sales.ts, 
      xlab = "Time", ylab = "Sales", 
@@ -31,7 +32,7 @@ sales.stl
 autoplot(sales.stl, main = "Gorcery Store Time Series Components")
 
 
-##Q3:
+##c):
 # Use Acf() function to identify autocorrelation and plot autocorrelation
 # for different lags (up to maximum of 12).
 autocor <- Acf(sales.ts, lag.max = 12, main = "Autocorrelation for Amtrak Ridership")
@@ -40,3 +41,17 @@ autocor <- Acf(sales.ts, lag.max = 12, main = "Autocorrelation for Amtrak Riders
 Lag <- round(autocor$lag, 0)
 ACF <- round(autocor$acf, 3)
 data.frame(Lag, ACF)
+
+
+## Q4
+## a)
+## CREATE DATA PARTITION.
+## PLOT DATA PARTITION.
+
+# Define the numbers of months in the training and validation sets,
+# nTrain and nValid, respectively.
+nValid <- 12
+nTrain <- length(sales.ts) - nValid
+train.ts <- window(sales.ts, start = c(2015, 1), end = c(2015, nTrain))
+valid.ts <- window(sales.ts, start = c(2015, nTrain + 1), 
+                   end = c(2015, nTrain + nValid))
