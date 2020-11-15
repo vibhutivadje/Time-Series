d7a486bfded4c2943f956a03ea673225e58456f5
vibhutivadje <vibhutivadje@gmail.com>
2020-11-15 15:53:36 -0800

    Time Series component

--- Assignment2.R
+++ Assignment2.R
@@ -15,3 +15,17 @@ revenue.ts <- ts(walmart.data$Revenue,
                start = c(2005, 1), end = c(2020, 2), freq = 4)
 
 revenue.ts
+
+##b):
+## Use plot() to plot time series data  
+plot(revenue.ts, 
+     xlab = "Time", ylab = "Revenue (in million $) ", 
+     ylim = c(70000, 150000), main = "Walmart Quaterly Revenue", col = "blue")
+
+# Use stl() function to plot times series components of the original data. 
+# The plot includes original data, trend, seasonal, and reminder 
+# (level and noise component).
+revenue.stl <- stl(revenue.ts, s.window = "periodic")
+revenue.stl
+autoplot(revenue.stl, main = "Gorcery Store Time Series Components")
+
