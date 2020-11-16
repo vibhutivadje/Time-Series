01bd4a932a00e3080fa015c53107208641153c54
vibhutivadje <vibhutivadje@gmail.com>
2020-11-15 16:45:34 -0800

    Data Spilt

--- Assignment2.R
+++ Assignment2.R
@@ -3,7 +3,7 @@
 install.packages("forecast")
 library(forecast)
 library(zoo)
-##Q1:
+#**************************************************Q1:*************************************************************#
 ##a)
 ## CREATE DATA FRAME. 
 walmart.data <- read.csv(file.choose(), header = TRUE)
@@ -22,10 +22,20 @@ plot(revenue.ts,
      xlab = "Time", ylab = "Revenue (in million $) ", 
      ylim = c(70000, 150000), main = "Walmart Quaterly Revenue", col = "blue")
 
+
 # Use stl() function to plot times series components of the original data. 
 # The plot includes original data, trend, seasonal, and reminder 
 # (level and noise component).
 revenue.stl <- stl(revenue.ts, s.window = "periodic")
 revenue.stl
-autoplot(revenue.stl, main = "Gorcery Store Time Series Components")
+autoplot(revenue.stl, main = "Walmart Revenue Time Series Components")
 
+#***************************************************Q2:*************************************************************#
+##a)
+#### CREATE DATA PARTITION.
+#Data Partiton with VAlidation partition of 16 periods and training partition of 46 periods
+nValid <- 16
+nTrain <- length(revenue.ts) - nValid
+train.ts <- window(revenue.ts, start = c(2005, 1), end = c(2005, nTrain))
+valid.ts <- window(revenue.ts, start = c(2005, nTrain + 1), 
+                   end = c(2005, nTrain + nValid))
