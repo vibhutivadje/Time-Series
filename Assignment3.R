69415b650208f241c97001e56d12cdfeb76c4710
vibhutivadje <vibhutivadje@gmail.com>
2020-11-29 15:31:41 -0800

    time series df

--- Assignment3.R
+++ Assignment3.R
@@ -3,5 +3,30 @@
 # Use forecast and zoo libraries
 library(forecast)
 library(zoo)
+
+##***************************************From Case2****************************************************##
+## CREATE DATA FRAME. 
+walmart.data <- read.csv(file.choose(), header = TRUE)
+# See the first 6 records of the file.
+head(walmart.data)
+
+## USE ts() FUNCTION TO CREATE TIME SERIES DATA SET.
+revenue.ts <- ts(walmart.data$Revenue, 
+                 start = c(2005, 1), end = c(2020, 2), freq = 4)
+
+revenue.ts
+
+##b):
+## Use plot() to plot time series data  
+plot(revenue.ts, 
+     xlab = "Time", ylab = "Revenue (in million $) ", 
+     ylim = c(70000, 150000), main = "Walmart Quaterly Revenue", col = "blue")
+
+# Use stl() function to plot times series components of the original data. 
+# The plot includes original data, trend, seasonal, and reminder 
+# (level and noise component).
+revenue.stl <- stl(revenue.ts, s.window = "periodic")
+revenue.stl
+autoplot(revenue.stl, main = "Walmart Revenue Time Series Components")
+
 #**************************************************Q1:*************************************************************#
-##a)
